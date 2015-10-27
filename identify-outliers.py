47044c2904cc87ae9dad663a528ea080771493d3
kalmar <kalmar@amu.edu.pl>
2015-10-27 11:57:38 +0100

    make-ified outlier identification (with decoupling for different models)

--- identify-outliers.py
+++ identify-outliers.py
@@ -1,5 +1,4 @@
 import os
-import random
 import sys
 
 import numpy as np
@@ -23,55 +22,15 @@ class DataPair:
     string      self.target_dir     (path) where to save all the points/outliers
     """
 
-    def __init__(self, filename, prefix, size=2000):
-        self.filename = filename
-        self.name = self.filename[:-4]
-        self.prepare_dirs(prefix)
-
-        self.raw_data = np.loadtxt(
-            os.path.join(self.initial_dir, 'pairs', self.filename))
-
-        if size < 1:
-            self.orig_points = self.raw_data
-        else:
-            if self.raw_data.shape[0] > size:
-                indexes = np.random.randint(0, self.raw_data.shape[0], size)
-                self.orig_points = self.raw_data[indexes]
-            else:
-                self.orig_points = self.raw_data
-
-        self.points = self.orig_points
+    def __init__(self, model):
+        self.current_dir = os.getcwd()
+        self.name = self.current_dir[-8:]
+        self.points = np.loadtxt(os.path.join(self.current_dir,
+                                                'orig_points'))
+        self.model = model
         self.dimension = self.points[0].shape[0]
 
         self.n_of_outliers = 15 * int(self.points.shape[0] / 100)
-
-    def prepare_dirs(self, prefix):
-        """file IO operations:
-            create target directory where all files will be saved."""
-
-        self.initial_dir = os.getcwd()
-        # print(self.initial_dir, prefix, self.name)
-        self.target_dir = os.path.join(self.initial_dir, prefix, self.name)
-        # os.mkdir(self.target_dir)
-
-    def linearise(self):
-        """Fit self.points to [0,1] interval in both coordinates"""
-        for i in range(self.dimension):
-            p = self.points[:, i]
-            maximum = np.max(p)
-            minimum = np.min(p)
-            p -= minimum
-            p /= (maximum - minimum)
-
-    def standardise(self):
-        """Standardise self.points, i.e.
-        mean = 0 and standard deviation = 1 in both dimensions"""
-        for i in range(self.dimension):
-            p = self.points[:, i]
-            mean = np.mean(p)
-            std = np.std(p)
-            p -= mean
-            p /= std
 
     def find_outliers_knn(self, k_nearest):
 
@@ -116,132 +75,38 @@ class DataPair:
             distances_vector[outlier] = ma.masked
         return self.outliers
 
-    def find_outliers(self, neighbours=0):
+    def find_outliers(self):
         """Procedure finding outliers based on nearest neighbours.
         if neighbours == 0 then all other points are taken into the account
         Outliers (their indexes in self.points) are stored in self.outliers"""
 
-        n = int(neighbours)
-        if n <= 0:  # outlier based on max distance to all others
+        if model == 'all':  # outlier based on max distance to all others
             self.outliers = self.find_outliers_all()
-            suffix = 'all'
-        else:
-            self.outliers = self.find_outliers_knn(neighbours)
-            suffix = 'knn'
-        self.save_outliers(suffix=suffix)
+        if model == 'knn':
+            nearest_neighbours = 2 * int(self.points.shape[0] / 100) + 2
+            self.outliers = self.find_outliers_knn(nearest_neighbours)
+
         print(self.name + ' Done with outliers!')
 
-    def save_outliers(self, suffix=''):
-        np.savetxt(os.path.join(self.target_dir, "outliers_" + str(suffix)),
+    def save_outliers(self):
+        np.savetxt(os.path.join(self.current_dir, "outliers_" + self.model),
                    np.asarray(self.outliers, dtype=int), fmt='%d')
 
-    def save_points_to_file(self, target_dir='', name="orig_points"):
-        """Saves numpy.array accesible under name attribute to
-        target_dir/name_self.filename"""
 
-        if target_dir == '':
-            target_dir = self.target_dir
+def workflow(model):
 
-        np.savetxt(os.path.join(target_dir, name),
-                   self.__getattribute__(name))
+    p = DataPair(model)
+    p.find_outliers()
+    p.save_outliers()
 
-    def save_cleaned_points(self, target_dir='', suffix=''):
-        """Saves the cleaned point into target_dir/cleaned_self.filename"""
-        if target_dir == '':
-            target_dir = self.target_dir
-
-        data = ma.masked_array(self.points)
-
-        for outlier in self.outliers:
-            data[outlier] = ma.masked
-        if suffix:
-            suffix = "_" + str(suffix)
-        compressed_data = data.compressed().reshape(
-            self.points.shape[0] - len(self.outliers),
-            self.dimension)
-        np.savetxt(os.path.join(target_dir,
-                                "cleaned_" + self.filename + suffix),
-                   compressed_data)
-
-    def plot_points_pdf(self, suffix=''):
-        print("Generating pdf with plots of ", self.name)
-        import matplotlib
-        matplotlib.use('Agg')
-        from matplotlib.backends.backend_pdf import PdfPages
-        import matplotlib.pyplot as plt
-
-        pdf_file = os.path.join(self.target_dir,
-                                self.filename[:-4] +
-                                '_' + str(suffix) + '.pdf')
-        with PdfPages(pdf_file) as pdf:
-            new_points = ma.masked_array(self.points)
-            for i in range(-1, len(self.outliers)):
-                # print(self.name[-2:0]+"-"+str(i), end=' ', flush=True)
-                plt.figure(figsize=(12, 12))
-                if i == -1:
-                    plt.title(self.name)
-                    plt.scatter(self.points[:, 0], self.points[:, 1],
-                                color='black', alpha=0.7, s=15)
-                else:
-                    outlier = self.outliers[i]
-                    removed_points = self.points[self.outliers[:i + 1]]
-                    new_points[outlier] = ma.masked
-                    to_plot = new_points.compressed().reshape(
-                        self.points.shape[0] - i - 1, 2)
-                    plt.figure(figsize=(12, 12))
-                    plt.title(self.name + ", outliers: " + str(i))
-                    plt.scatter(to_plot[:, 0], to_plot[:, 1],
-                                color='black', alpha=0.7, s=15)
-                    plt.scatter(removed_points[:, 0], removed_points[:, 1],
-                                color='red', alpha=0.7, s=15)
-                pdf.savefig()
-                plt.close()
-        print(self.name, "Done pdf!")
-
-
-def workflow(prefix, filename, size):
-    prefix_dir = os.path.join(os.getcwd(), prefix)
-    p = DataPair(filename, prefix, size)
-    if p.dimension == 2:
-        p.standardise()
-        p.save_points_to_file()
-
-        p.find_outliers(neighbours=0)
-        # if p.dimension == 2:
-        #     p.plot_points_pdf(suffix='all')
-
-        p.find_outliers(neighbours=2 * int(p.orig_points.shape[0] / 100))
-        # if p.dimension == 2:
-        #     p.plot_points_pdf(suffix='knn')
-
-        # else:
-        #     print(p.name, p.dimension, "is too many to plot!")
-        # done_file = os.path.join(prefix_dir, filename[:-4], "done")
-        # print("Creating file", done_file)
-        # os.mknod(done_file)
-        return 0
-    else:
-        return 1
 
 if __name__ == "__main__":
 
-    random.seed(0)
-
-    blacklist = ['pair0023.txt',
-                 'pair0033.txt',
-                 'pair0037.txt',
-                 'pair0047.txt',
-                 'pair0070.txt']
-
-    if len(sys.argv) < 3:
-        print("Usage: identify-outliers.py $PREFIX $FILENAME")
+    if len(sys.argv) < 1:
+        print("Usage: identify-outliers.py $MODEL")
     else:
-        prefix = sys.argv[1]
-        file = sys.argv[2]
-        if file not in blacklist:
-            workflow(prefix, file, size=1000)
-        else:
-            print(file, "is blacklisted! (it doesn't fit the model?)")
+        model = sys.argv[1]
+        workflow(model)
 #
 # def main(prefix, size=2000, jobs=1):
 #
