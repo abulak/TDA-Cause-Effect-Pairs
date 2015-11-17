61fa7e9b165fe340294db0096f8c8cfe4a6b2b99
kalmar <kalmar@amu.edu.pl>
2015-11-17 03:02:56 +0000

    cpu_count/2 in pair-multiprocessing seems ok with -j3 in pairs-generator

--- pairs-generator.py
+++ pairs-generator.py
@@ -1,10 +1,7 @@
 import subprocess
 
-for i in range(1, 2):
+for i in range(1, 21):
     prefix = '{0:03d}'.format(i)
     print("Starting ", prefix, end=' ')
-    subprocess.call(["make", "-j1", "knn", "PREFIX="+prefix, "SIZE=2000"])
+    subprocess.call(["make", "-j3", "knn", "PREFIX="+prefix, "SIZE=2000"])
     print("done", prefix)
-
-
-
