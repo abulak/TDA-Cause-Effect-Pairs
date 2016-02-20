21bc15c9d61242d413fad6dcac39f998f87e05c3
Marek Kaluba <kalmar@amu.edu.pl>
2016-02-20 00:48:44 +0000

    New fancy persistence plot

--- PlottingUtilities.py
+++ PlottingUtilities.py
@@ -12,6 +12,8 @@ import numpy.ma as ma
 import GeometricComplex as GC
 import matplotlib.pyplot as plt
 
+import seaborn as sns
+
 from matplotlib.collections import LineCollection, PolyCollection
 
 
@@ -56,20 +58,25 @@ class TopologyPlotter:
 
         minimal = self.extrema[i]['minima'][axis]
         maximal = self.extrema[i]['maxima'][axis]
-        mpl_axis.set_aspect(1)
+
+        mpl_axis.set_aspect('equal')
+        if inverted:
+            minimal *= -1
+            maximal *= -1
+
         mpl_axis.plot([minimal, maximal], [minimal, maximal],
                       color='black', alpha=0.5)
+
         points = np.array(self.diagrams[i][filtration][0])
 
         if points.size:  # if the array is not empty...
-            if inverted:
-                points *= -1
 
             mpl_axis.scatter(points[:, 0], points[:, 1],
-                             marker='+', s=300, color='r',
-                             linewidth=1.5)
+                             marker='x', s=100, color='r',
+                             linewidth=2)
             for pt in points:
-                mpl_axis.plot([pt[0], pt[0]], [pt[0], pt[1]],
+                mpl_axis.plot(np.array([pt[0], pt[0]]),
+                              np.array([pt[0], pt[1]]),
                               color='black', alpha=0.3)
         mpl_axis.text(0.5, 0.1, "Filtration: " + filtration,
                       transform=mpl_axis.transAxes)
@@ -95,37 +102,47 @@ class TopologyPlotter:
                   "for the pair!")
         else:
             fig = plt.gcf()
-
-            central = fig.add_subplot(335, zorder=10)
-
-            central.set_aspect('equal')
+            cmplx = fig.add_subplot(2,3,1)
+            cmplx.set_aspect('equal')
             self.plot_delaunay(i)
-            central.tick_params(labeltop='on', labelright='on')
 
-            top = fig.add_subplot(332, sharex=central)
-            bottom = fig.add_subplot(338, sharex=central)
-            left = fig.add_subplot(334, sharey=central)
-            right = fig.add_subplot(336, sharey=central)
+            cmplx.tick_params(labeltop='on', labelleft='on',
+                              labelright='off', labelbottom='off')
 
-            for ax in [top, bottom, left, right]:
+            inv_cmplx = fig.add_subplot(2,3,6)
+            inv_cmplx.set_aspect('equal')
+            inv_cmplx.tick_params(labeltop='on', labelright='on')
+            inv_cmplx.tick_params(labeltop='off', labelleft='off',
+                                  labelright='on', labelbottom='on')
+            self.points *= -1
+            self.plot_delaunay(i)
+
+            with sns.axes_style("whitegrid"):
+                y_inc = fig.add_subplot(232, sharey=cmplx)
+                x_inc = fig.add_subplot(234, sharex=cmplx)
+                self.plot_diagram(i, mpl_axis=x_inc, filtration="X", axis=0)
+                self.plot_diagram(i, mpl_axis=y_inc, filtration="Y", axis=1)
+
+                x_dec = fig.add_subplot(233, sharex=inv_cmplx)
+                y_dec = fig.add_subplot(235, sharey=inv_cmplx)
+
+                self.plot_diagram(i, mpl_axis=x_dec, filtration="X_inverted", axis=0, inverted=True)
+                self.plot_diagram(i, mpl_axis=y_dec, filtration="Y_inverted", axis=1, inverted=True)
+
+
+            for ax in [x_inc, y_inc, x_dec, y_dec]:
                 for label in ax.get_xticklabels():
                     label.set_visible(False)
                 for label in ax.get_yticklabels():
                     label.set_visible(False)
-
-            #     ax.set_yticklabels([])
-            right.invert_xaxis()
-            self.plot_diagram(i, mpl_axis=top, filtration="X", axis=0)
-
-            self.plot_diagram(i, mpl_axis=left, filtration="Y", axis=1)
-            self.plot_diagram(i, mpl_axis=right,
-                              filtration="Y_inverted", axis=1, inverted=True)
-            # axs[2,0].set_axis_off()
-            self.plot_diagram(i, mpl_axis=bottom,
-                              filtration="X_inverted", axis=0, inverted=True)
-
             plt.tight_layout()
             fig.subplots_adjust(wspace=0, hspace=0)
+
+            for ax in [x_dec, y_dec, inv_cmplx]:
+                pos1 = ax.get_position()
+                pos2 = [pos1.x0 + 0.02, pos1.y0 - 0.03, pos1.width, pos1.height]
+                ax.set_position(pos2)
+
             return fig
 
 
